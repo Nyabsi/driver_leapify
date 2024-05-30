1e888b74d76aa7dae11a4cc25a6896ffb6a064a3
Nyabsi <nyabsi@sovellus.cc>
2024-05-30 17:04:47 +0300

    controller: bind Joycon inputs properly to the controller

--- driver_leap/Controller/CJoyconInput.cpp
+++ driver_leap/Controller/CJoyconInput.cpp
@@ -27,9 +27,6 @@ bool CJoyconInput::IsConnected()
 	int* handles = new int[m_deviceCount];
 	JslGetConnectedDeviceHandles(handles, m_deviceCount);
 
-    m_joycons[0].connected = false;
-    m_joycons[1].connected = false;
-
     for (int i = 0; i < m_deviceCount; i++)
     {
         int handle = handles[i];
@@ -70,16 +67,29 @@ void CJoyconInput::Update(CLeapIndexController* left, CLeapIndexController* righ
         left->SetButtonState(CLeapIndexController::IndexButton::IB_SystemClick, state.buttons & 0x20000);
 
         // A
-        left->SetButtonState(CLeapIndexController::IndexButton::IB_SystemTouch, state.buttons & 0x00002);
-        left->SetButtonState(CLeapIndexController::IndexButton::IB_SystemClick, state.buttons & 0x00002);
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_AClick, state.buttons & 0x00002);
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_ATouch, state.buttons & 0x00002);
 
         // B
-        left->SetButtonState(CLeapIndexController::IndexButton::IB_SystemTouch, state.buttons & 0x00001);
-        left->SetButtonState(CLeapIndexController::IndexButton::IB_SystemClick, state.buttons & 0x00001);
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_BClick, state.buttons & 0x00001);
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_BClick, state.buttons & 0x00001);
 
         // Joystick XY
-        left->SetButtonState(CLeapIndexController::IndexButton::IB_ThumbstickX, state.stickLX);
-        left->SetButtonState(CLeapIndexController::IndexButton::IB_ThumbstickY, state.stickLY);
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_ThumbstickClick, state.buttons & 0x00040);
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_ThumbstickTouch, state.stickLX > 0 && state.stickLY > 0);
+        left->SetButtonValue(CLeapIndexController::IndexButton::IB_ThumbstickX, state.stickLX);
+        left->SetButtonValue(CLeapIndexController::IndexButton::IB_ThumbstickY, state.stickLY);
+
+        // Trigger
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_TriggerClick, state.buttons & 0x00400);
+        left->SetButtonValue(CLeapIndexController::IndexButton::IB_TriggerValue, state.lTrigger);
+
+        // Grip
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_GripTouch, state.buttons & 0x00100);
+
+        // TODO: handle float (for DualShock, DualSense)
+        left->SetButtonValue(CLeapIndexController::IndexButton::IB_GripValue, state.buttons & 0x00100 ? 1.0f : 0.0f);
+        left->SetButtonValue(CLeapIndexController::IndexButton::IB_GripForce, state.buttons & 0x00100 ? 1.0f : 0.0f);
     }
 
     if (m_joycons[1].connected) // Right Hand
@@ -91,15 +101,29 @@ void CJoyconInput::Update(CLeapIndexController* left, CLeapIndexController* righ
         right->SetButtonState(CLeapIndexController::IndexButton::IB_SystemClick, state.buttons & 0x10000);
 
         // A
-        right->SetButtonState(CLeapIndexController::IndexButton::IB_SystemTouch, state.buttons & 0x01000);
-        right->SetButtonState(CLeapIndexController::IndexButton::IB_SystemClick, state.buttons & 0x01000);
+        right->SetButtonState(CLeapIndexController::IndexButton::IB_AClick, state.buttons & 0x01000);
+        right->SetButtonState(CLeapIndexController::IndexButton::IB_AClick, state.buttons & 0x01000);
 
         // B
-        right->SetButtonState(CLeapIndexController::IndexButton::IB_SystemTouch, state.buttons & 0x08000);
-        right->SetButtonState(CLeapIndexController::IndexButton::IB_SystemClick, state.buttons & 0x08000);
+        right->SetButtonState(CLeapIndexController::IndexButton::IB_BClick, state.buttons & 0x08000);
+        right->SetButtonState(CLeapIndexController::IndexButton::IB_BClick, state.buttons & 0x08000);
 
         // Joystick XY
-        right->SetButtonState(CLeapIndexController::IndexButton::IB_ThumbstickX, state.stickRX);
-        right->SetButtonState(CLeapIndexController::IndexButton::IB_ThumbstickY, state.stickRY);
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_ThumbstickClick, state.buttons & 0x00080);
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_ThumbstickTouch, state.stickRX > 0 && state.stickRY > 0);
+        right->SetButtonValue(CLeapIndexController::IndexButton::IB_ThumbstickX, state.stickRX);
+        right->SetButtonValue(CLeapIndexController::IndexButton::IB_ThumbstickY, state.stickRY);
+
+        // Trigger
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_TriggerClick, state.buttons & 0x00800);
+        left->SetButtonValue(CLeapIndexController::IndexButton::IB_TriggerValue, state.rTrigger);
+
+        // Grip
+        left->SetButtonState(CLeapIndexController::IndexButton::IB_GripTouch, state.buttons & 0x00200);
+        left->SetButtonValue(CLeapIndexController::IndexButton::IB_GripValue, state.buttons & 0x00200 ? 1.0f : 0.0f);
+        left->SetButtonValue(CLeapIndexController::IndexButton::IB_GripForce, state.buttons & 0x00200 ? 1.0f : 0.0f);
     }
+
+    m_joycons[0].connected = false;
+    m_joycons[1].connected = false;
 }
