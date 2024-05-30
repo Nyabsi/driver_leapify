28a10063827f18bab2af044006aa523ef9385909
Nyabsi <nyabsi@sovellus.cc>
2024-05-30 18:40:53 +0300

    main: make HmdDriverFactory cross-platform

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <Windows.h>
+
+#include "Core/CServerDriver.h"
+
+CServerDriver g_serverDriver;
+
+extern "C" __declspec(dllexport) void* HmdDriverFactory(const char *pInterfaceName, int *pReturnCode)
+{
+    void *l_result = nullptr;
+    if(!strcmp(vr::IServerTrackedDeviceProvider_Version, pInterfaceName)) l_result = dynamic_cast<vr::IServerTrackedDeviceProvider*>(&g_serverDriver);
+    else
+    {
+        if(pReturnCode) *pReturnCode = vr::VRInitError_Init_InterfaceNotFound;
+    }
+    return l_result;
+}
