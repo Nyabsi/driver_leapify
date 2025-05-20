d9f624afd6e1c84fa13460f09ab6d19ec137f3dd
Nyabsi <nyabsi@sovellus.cc>
2025-05-20 12:18:09 +0300

    initial device registration

--- src/core/StateManager.hpp
+++ src/core/StateManager.hpp
@@ -3,92 +3,51 @@
 #include <util/Singleton.hpp>
 #include <openvr_driver.h>
 
+#include <utility>
 #include <vector>
 #include <map>
 
-#include <util/SimpleKalmanFilter.hpp>
+// #include <util/SimpleKalmanFilter.hpp>
 
+/*
 struct TransformHook {
 	vr::VRInputComponentHandle_t handle;
 	vr::ETrackedControllerRole role;
 };
+*/
 
-struct ControllerState {
-	bool isIdle{ false };
-	int64_t timestamp{ -1 };
-	SimpleKalmanFilter m_velocity{};
+struct DeviceState {
+	std::string manufacturerName { "Undefined" };
+	std::string trackingSystem { "Undefined" };
+	int32_t deviceClass { -1 };
+	int32_t roleHint { -1 };
+	bool shouldWeBlockPose { false };
 };
 
 class StateManager {
+private:
+	std::map<uint32_t, DeviceState> m_devices {};
 public:
 	static inline StateManager& Get() { return Singleton<StateManager>::get(); }
-	vr::VRBoneTransform_t* getLeapTransform(vr::ETrackedControllerRole role) const
-	{ 
-		if (role == vr::TrackedControllerRole_LeftHand)
-			return m_passThroughTransformLeft;
-		if (role == vr::TrackedControllerRole_RightHand)
-			return m_passThroughTransformRight;
 
-		return nullptr;
+	bool IsDeviceRegistered(uint32_t deviceId) {
+		return m_devices.count(deviceId) > 0;
 	}
 
-	void setLeapTransform(vr::VRBoneTransform_t* transform, vr::ETrackedControllerRole role) 
-	{ 
-		if (role == vr::TrackedControllerRole_LeftHand)
-			m_passThroughTransformLeft = transform;
-		else
-			m_passThroughTransformRight = transform;
+	void RegisterDevice(uint32_t deviceId, DeviceState device) {
+		m_devices[deviceId] = device;
 	}
 
-	void addTransformHook(vr::VRInputComponentHandle_t handle, vr::ETrackedControllerRole role) 
-	{
-		TransformHook hook {};
-		hook.handle = handle;
-		hook.role = role;
-
-		m_transformHooks.push_back(hook);
-	}
-
-	vr::ETrackedControllerRole getTransformHookRole(vr::VRInputComponentHandle_t handle)
-	{
-		for (auto& hook : m_transformHooks)
-		{
-			if (hook.handle == handle)
-				return hook.role;
+	std::vector<std::pair<uint32_t, DeviceState>> GetDeviceByClass(int32_t deviceClass) {
+		std::vector<std::pair<uint32_t, DeviceState>> result = {};
+		for (auto& [id, device] : m_devices) {
+			if (device.deviceClass == deviceClass)
+				result.push_back(std::make_pair(id, device));
 		}
-
-		return vr::TrackedControllerRole_Invalid;
+		return result;
 	}
 
-	void clearTransformHooks() { m_transformHooks.clear(); }
-
-	void updateControllerState(int device, ControllerState newState) { m_controllerStates[device] = newState; }
-	std::map<int, ControllerState> getControllerStates() { return m_controllerStates; }
-	ControllerState& getState(int device) {
-		return m_controllerStates[device];
+	bool ShouldWeBlockDevicePose(uint32_t deviceId) {
+		return m_devices[deviceId].shouldWeBlockPose;
 	}
-
-	vr::DriverPose_t getLeapPose(vr::ETrackedControllerRole role) const
-	{ 
-		if (role == vr::TrackedControllerRole_LeftHand)
-			return m_passthroughPoseLeft;
-		if (role == vr::TrackedControllerRole_RightHand)
-			return m_passthroughPoseRight;
-		return vr::DriverPose_t();
-	}
-
-	void setLeapPose(vr::DriverPose_t pose, vr::ETrackedControllerRole role)
-	{ 
-		if (role == vr::TrackedControllerRole_LeftHand)
-			m_passthroughPoseLeft = pose;
-		else
-			m_passthroughPoseRight = pose;
-	}
-private:
-	vr::VRBoneTransform_t* m_passThroughTransformLeft { nullptr };
-	vr::VRBoneTransform_t* m_passThroughTransformRight { nullptr };
-	std::vector<TransformHook> m_transformHooks { };
-	std::map<int, ControllerState> m_controllerStates{ };
-	vr::DriverPose_t m_passthroughPoseLeft { };
-	vr::DriverPose_t m_passthroughPoseRight { };
 };
--- src/hook/hooks/IVRServerDriverHost_006.hpp
+++ src/hook/hooks/IVRServerDriverHost_006.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <core/StateManager.h>
+
 #ifdef _WIN32
 #include <Windows.h>
 #endif
@@ -13,6 +15,29 @@ namespace hooks {
         }
 
         bool TrackedDevicePoseUpdated(uint32_t& unWhichDevice, const vr::DriverPose_t& newPose, uint32_t& unPoseStructSize) {
+
+            if (!StateManager::Get().IsDeviceRegistered(unWhichDevice))
+            {
+                auto props = vr::VRProperties()->TrackedDeviceToPropertyContainer(unWhichDevice);
+
+                DeviceState device {};
+                device.manufacturerName = vr::VRProperties()->GetStringProperty(props, vr::ETrackedDeviceProperty::Prop_ManufacturerName_String);
+                device.trackingSystem = vr::VRProperties()->GetStringProperty(props, vr::ETrackedDeviceProperty::Prop_TrackingSystemName_String);
+                device.deviceClass = vr::VRProperties()->GetInt32Property(props, vr::ETrackedDeviceProperty::Prop_DeviceClass_Int32);
+                device.roleHint = vr::VRProperties()->GetInt32Property(props, vr::ETrackedDeviceProperty::Prop_ControllerRoleHint_Int32);
+
+                StateManager::Get().RegisterDevice(unWhichDevice, device);
+
+                #ifdef _WIN32
+                MessageBoxA(NULL, "The sea was sailing and we did init register on the controller", "Validation", MB_OK);
+                #endif
+            }
+
+            if (StateManager::Get().ShouldWeBlockDevicePose(unWhichDevice))
+            {
+                newPose.deviceIsConnected = false;
+            }
+
             return false;
         }
     };
