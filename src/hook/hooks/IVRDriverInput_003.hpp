7e17f10b93e4280db563cc44720ef22469899deb
Nyabsi <nyabsi@sovellus.cc>
2025-05-20 10:40:01 +0300

    uh, textbox needs buttons

--- src/hook/hooks/IVRDriverInput_003.hpp
+++ src/hook/hooks/IVRDriverInput_003.hpp
@@ -11,20 +11,20 @@ namespace hooks {
         // note: idx 1
         bool UpdateBooleanComponent(vr::VRInputComponentHandle_t& ulComponent, bool& bNewValue, double& fTimeOffset) {
             #ifdef _WIN32
-            MessageBoxA(NULL, "UpdateBooleanComponent OK", "Validation");
+            MessageBoxA(NULL, "UpdateBooleanComponent OK", "Validation", MB_OK);
             #endif
             return false;
         }
 
         void CreateSkeletonComponent(vr::PropertyContainerHandle_t& ulContainer, const char*& pchName, const char*& pchSkeletonPath, const char*& pchBasePosePath, vr::EVRSkeletalTrackingLevel& eSkeletalTrackingLevel, const vr::VRBoneTransform_t*& pGripLimitTransforms, uint32_t& unGripLimitTransformCount, vr::VRInputComponentHandle_t*& pHandle) {
             #ifdef _WIN32
-            MessageBoxA(NULL, "CreateSkeletonComponent OK", "Validation");
+            MessageBoxA(NULL, "CreateSkeletonComponent OK", "Validation", MB_OK);
             #endif
         }
 
 		bool UpdateSkeletonComponent(vr::VRInputComponentHandle_t& ulComponent, vr::EVRSkeletalMotionRange& eMotionRange, const vr::VRBoneTransform_t*& pTransforms, uint32_t& unTransformCount) {
             #ifdef _WIN32
-            MessageBoxA(NULL, "UpdateSkeletonComponent OK", "Validation");
+            MessageBoxA(NULL, "UpdateSkeletonComponent OK", "Validation", MB_OK);
             #endif
             return false;
         }
